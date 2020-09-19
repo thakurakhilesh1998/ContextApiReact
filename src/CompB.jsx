66813608a71864ddf4e7a02c8cdc8dccc86c3e7b
thakurakhilesh1998 <39956027+thakurakhilesh1998@users.noreply.github.com>
2020-09-19 20:35:57 +0530

    use context

--- src/CompB.jsx
+++ src/CompB.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import {CompC} from "./CompC";
+import {FirstName,LastName} from './App';
 
 const CompB=()=>{
-    return(<CompC/>);
+    const fname=useContext(FirstName);
+    const lname=useContext(LastName);
+    return(
+            <>
+            <h2>My first name is :{fname}
+            </h2>
+            <h3>My lastname is:{lname}</h3>
+            <CompC/>
+            </>);
 }
 
 export {CompB};
